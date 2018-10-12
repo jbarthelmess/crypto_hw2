bcb2f34a2f53b580378b452a1cf7923534ef41ed
Josh Barthelmess <joshbarthelmess3@gmail.com>
2018-10-11 23:28:40 -0400

    Finished very basic working

---
+++ hw2.c
@@ -0,0 +1,254 @@
+#include<stdio.h>
+#include "crypto_math.h"
+#include<string.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<arpa/inet.h>
+#include "../HW1/HW1.h"
+#include<unistd.h>
+
+void do_encrypt(char* plain, char* dest, int num_bytes, int key)
+{
+    int i = 0;
+    while(i < num_bytes)
+    {
+        int plain_text = plain[i] & 255;
+        int result = encrypt(plain_text, key);
+        dest[i] = result & 255;
+        i++;
+    }
+}
+
+void do_decrypt(char* plain, char* dest, int num_bytes, int key)
+{
+    int i = 0;
+    while(i < num_bytes)
+    {
+        int encrypt_text = plain[i] & 255;
+        int result = decrypt(encrypt_text, key);
+        dest[i] = result & 255;
+        //printf("DECRYPTING %d to %d using %d\n", encrypt_text, result, key);
+        i++;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    int top = 0;
+    int q;
+    printf("%d\n", argc);
+    if(argc < 4)
+    {
+        fprintf(stderr, "USAGE: ./a.out <NAME> <PORT> <KDC_PORT> [<CONNECTOR>]\n");
+        return EXIT_FAILURE;
+    }
+    srand(strlen(argv[1])+argc);
+    // First connect to KDC 
+    int kdc = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in kdc_info;
+    memset(&kdc_info, 0, sizeof(kdc_info));
+    kdc_info.sin_family = AF_INET;
+    kdc_info.sin_port = htons(atoi(argv[3]));
+    inet_pton(AF_INET, "127.0.0.1", &kdc_info.sin_addr);
+    
+    top = connect(kdc, (struct sockaddr* ) &kdc_info, sizeof(kdc_info));
+    if(top == -1)
+    {
+        perror("connect");
+        return EXIT_FAILURE;
+    }
+    printf("GOT HERE\n");
+    
+    char name[256]; // HOLDS MY NAME
+    unsigned char buf[1024]; // BUFFER FOR SEND AND RECV
+    unsigned char encrypt_buf[1024];
+    if(strlen(argv[1]) > 255)
+    {
+        fprintf(stderr, "NAME IS TOO LONG\n");
+        return EXIT_FAILURE;
+    }
+    printf("NAME VERIFIED\n");
+    // SEND KDC MY NAME AND IT WILL SEND DIFFIE HELMAN INFO
+    strcpy(name, argv[1]);
+    printf("COPIED NAME\n");
+    top = send(kdc, name, strlen(name), 0);
+    if(top == -1)
+    {
+        perror("send");
+        exit(EXIT_FAILURE);
+    }
+    printf("NAME SENT\n");
+    top = recv(kdc, buf, 12, 0); // RECV PRIME, ROOT, KDC_PUB All 32 bit ints
+    if(top == -1)
+    {
+        perror("recv");
+        exit(EXIT_FAILURE);
+    }
+    printf("%d\n", top);
+    for(q = 0; q< top; q++)
+    {
+        printf("%u ", buf[q]);
+    }
+    printf("\n");
+    // CREATE KEY
+    u_int prime, root, kdc_pub;
+    memcpy(&prime, buf, 4);
+    memcpy(&root, &buf[4], 4);
+    memcpy(&kdc_pub, &buf[8], 4);
+    
+    printf("RECIEVED KEY INFO\n");
+    printf("%u\n", prime);
+    /* PERFORM DIFFIE HELMAN TRICK, secret key is only used to get symmetric key*/
+    u_int secret = rand() % prime; // Must be less than the prime
+    u_int my_pub = exp_mod(root, secret, prime);
+    memcpy(buf, &my_pub, 4);
+    
+    send(kdc, buf, 4, 0); // SEND PUBLIC KEY
+    u_int key = exp_mod(kdc_pub, secret, prime); // CALCULATE SYMMETRIC KEY
+    printf("CREATED KEY WITH SERVER: %u\n", key);
+    
+    key = key & 1023; // KEYS ARE 10 bits for TOY_DES
+    printf("ACTUAL DECRYPTION KEY: %u\n", key);
+    
+    int connection = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in init;
+    struct sockaddr_in other;
+    memset(&init, 0, sizeof(init));
+    memset(&other, 0, sizeof(other));
+    socklen_t throwaway;
+    int byte_count;
+    int i;
+    char other_name[256];
+    u_int key_s;
+    char input_check;
+    
+    if(argc == 5) // This will be the initiator of the convo
+    {
+        // Allows time to set up reciever if needed
+        printf("PLEASE INPUT ANYTHING WHEN YOU ARE READY TO CONTINUE: ");
+        scanf("%c", &input_check);
+        
+        // SEND UNENCRYPTED MESSAGE THAT I WANT TO TALK TO BOB
+        printf("GETTING SESSION KEY FROM KDC\n");
+        u_int nonce1 = rand();
+        u_int check = 0;
+        strcpy(other_name, argv[4]);
+        strcpy(buf, name);
+        strcpy(&buf[strlen(name)+1], other_name);
+        memcpy(&buf[strlen(name)+strlen(other_name)+2], (unsigned char *)&nonce1, 4);
+        byte_count = send(kdc, buf, strlen(name)+strlen(other_name)+6, 0);
+        printf("SENT %d bytes\n", byte_count);
+        // RECIEVE ENCRYPTED PACKETS
+        byte_count = recv(kdc, encrypt_buf, strlen(name)+strlen(other_name)+18, 0);
+        fflush(stdout);
+        do_decrypt(encrypt_buf, buf, byte_count, key);
+        
+        // SAVE SESSION KEY AND DO NONCE CHECK
+        memcpy(&key_s, buf, 4);
+        memcpy(&check, &buf[strlen(other_name)+5], 4);
+        if(check == nonce1)
+            printf("NONCE CHECK SUCCESSFUL, SENDING TO %s\n", other_name);
+        else
+        {
+            printf("NONCE CHECK UNSUCCESSFUL\nNONCE: %u\nNONCE_RET: %u\n", nonce1, check);
+            exit(EXIT_FAILURE);
+        }
+        
+        // CONNECT TO RECIEVER AND SEND ENCRYPTED PACKET
+        unsigned char other_buf[1024];
+        memcpy(other_buf, &buf[strlen(other_name)+9], strlen(name)+9);
+        
+        init.sin_port = htons(atoi(argv[2]));
+        init.sin_family = AF_INET;
+        inet_pton(AF_INET, "127.0.0.1", &init.sin_addr);
+        printf("SETTING UP CONNECTION TO %s\n", other_name);
+        top = connect(connection, (struct sockaddr* ) &init, sizeof(init));
+        if(top == -1)
+        {
+            perror("connect");
+            exit(EXIT_FAILURE);
+        }
+        send(connection, other_buf, strlen(name)+9, 0);
+        printf("SENT ENCRYPTED PACKET, WAITING FOR RESPONSE\n");
+        
+        // RECEIVE SECOND NONCE PERFORM BITSHIFT AND SEND IT BACK ENCRYPTED
+        recv(connection, buf, 4, 0);
+        for(i = 0; i< 4; i++)
+        {
+            buf[i] = decrypt(buf[i], key_s);
+        }
+        memcpy(&check, buf, 4);
+        u_int holder2 = 240;
+        holder2 = holder2 << 24;
+        holder2 = holder2 & check;
+        check = check << 4;
+        holder2 = holder2 >> 28;
+        check = check + holder2;
+        memcpy(buf, &check, 4);
+        for(i = 0; i< 4; i++)
+        {
+            buf[i] = encrypt(buf[i], key_s);
+        }
+        printf("SENDING SHIFTED NONCE, SETUP COMPLETE\n");
+        send(connection, buf, 4, 0);
+    }
+    else // This person will wait to be contacted
+    {
+        // SET UP LISTENING CONNECTION
+        init.sin_port = htons(atoi(argv[2]));
+        init.sin_family = AF_INET;
+        inet_pton(AF_INET, "127.0.0.1", &init.sin_addr);
+        
+        bind(connection, (struct sockaddr* ) &init, sizeof(init));
+        listen(connection, 2);
+        printf("READY TO CONNECT TO OTHERS ON PORT %hu\n", atoi(argv[2]));
+        int comm = accept(connection, (struct sockaddr*) &other, &throwaway);
+        close(connection);
+        
+        // RECIEVE ENCRYPTED PACKET
+        byte_count = recv(comm, buf, 264, 0);
+        for(i = 0; i< byte_count; i++)
+        {
+            buf[i] = 255 & decrypt(buf[i], key);
+        }
+        
+        memcpy(&key_s, buf, 4);
+        key_s = key_s & 1023;
+        
+        strcpy(other_name, &buf[4]);
+        printf("CONTACTED BY %s performing final verification\n", other_name);
+        
+        u_int nonce = rand();
+        u_int nonce_ret;
+        u_int holder;
+        memcpy(buf, &nonce, 4);
+        for(i=0; i< 4; i++)
+        {
+            buf[i] = 255 & encrypt(buf[i], key_s);
+        }
+        
+        send(comm, buf, 4, 0);
+        
+        recv(comm, buf, 4, 0);
+        
+        for(i = 0; i< 4; i++)
+        {
+            buf[i] = 255 & decrypt(buf[i], key_s);
+        }
+        memcpy(&nonce_ret, buf, 4);
+        printf("RECEIVED NONCE_RET: %u\n", nonce_ret);
+        
+        holder = nonce_ret & 15;
+        
+        nonce_ret = nonce_ret >> 4;
+        nonce_ret = nonce_ret + (holder << 28);
+        
+        if(nonce_ret == nonce)
+            printf("VERIFIED\n");
+        else
+        {
+            printf("VERIFICATION FAILED\nNONCE: %u\nNONCE_RET: %u\n", nonce, nonce_ret);
+        }
+    }
+    return 0;
+}
---
+++ kdc.c
@@ -0,0 +1,216 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<string.h>
+#include "crypto_math.h"
+#include "../HW1/HW1.h"
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<sys/select.h>
+#include<sys/types.h>
+#include<sys/time.h>
+
+void do_encrypt(char* plain, char* dest, int num_bytes, int key)
+{
+    int i = 0;
+    while(i < num_bytes)
+    {
+        int plain_text = plain[i] & 255;
+        int result = encrypt(plain_text, key);
+        dest[i] = result & 255;
+        //printf("ENCRYPTED %d to %d Using %d\n", plain_text, result, key);
+        i++;
+    }
+}
+
+void do_decrypt(char* plain, char* dest, int num_bytes, int key)
+{
+    int i = 0;
+    while(i < num_bytes)
+    {
+        int encrypt_text = plain[i] & 255;
+        int result = decrypt(encrypt_text, key);
+        dest[i] = result & 255;
+        i++;
+    }
+}
+
+u_int make_key(char** name_buf, int comm_sock, u_int p, u_int root)
+{
+    u_int secret = rand() % p;
+    u_int public = exp_mod(root, secret, p);
+    u_int other_pub;
+    unsigned char buf[524];
+    unsigned char encrypt_buf[1024];
+    char name[256];
+    int byte_count = recv(comm_sock, name, 255, 0);
+    name[byte_count]  = '\0';
+    printf("RECIEVED CONNECTION FROM %s\n", name);
+    memcpy(buf, &p, 4);
+    memcpy(&buf[4], &root, 4);
+    memcpy(&buf[8], &public, 4);
+    
+    byte_count = send(comm_sock, buf, 12, 0);
+    
+    byte_count = recv(comm_sock, &other_pub, 4, 0);
+    
+    u_int key = exp_mod(other_pub, secret, p);
+    printf("CREATED KEY with %s: %u\n", name, key);
+    strcpy(*name_buf, name);
+    return key;
+}
+
+int main()
+{
+    int j;
+    srand(787);
+    // printf("GOT HERE\n");
+    u_int prime = generate_prime(0);
+    int * factors;
+    int size;
+    u_int p_root = find_prime_root(prime, &factors, &size);
+    u_int otherPrime;
+    prime_root_find:
+    //printf("GENERATED PRIME\n");
+    otherPrime = generate_prime(prime);
+    int k;
+    for(k = 0; k < size; k++)
+    {
+        if(otherPrime == factors[k])
+        {
+            goto prime_root_find;
+        }
+    }
+    p_root = exp_mod(p_root, otherPrime, prime);
+    //printf("GENERATED PRIMITIVE ROOT\n");
+    
+    unsigned char buf[1024];
+    int c = 0;
+    int i;
+    int byte_count;
+    socklen_t stuff;
+    int listener;
+    int socks[2];
+    //int pipefd[2];
+    struct sockaddr_in user[2];
+    struct sockaddr_in self;
+    listener = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&self, '\0', sizeof(self));
+    self.sin_family = AF_INET;
+    self.sin_port = htons(4625);
+    inet_pton(AF_INET, "127.0.0.1", &self.sin_addr);
+    
+    bind(listener, (struct sockaddr*) &self, sizeof(self));
+    listen(listener, 2);
+    //pipe(pipefd);
+    
+    char* names[2];
+    u_int keys[2];
+    for(i = 0; i<2; i++)
+        names[i] = malloc(256);
+    
+    while(c < 2) // Accept communicating parties and create keys using diffie helman
+    {
+        printf("READY TO ACCEPT CONNECTIONS\n");
+        socks[c] = accept(listener, (struct sockaddr*) &user[c], &stuff);
+        
+        //int p = fork();
+        //if(p == 0)
+        //{
+        //    close(pipefd[0]);
+        keys[c] = 1023 & make_key(&names[c], socks[c], prime, p_root);
+        //}
+        c++;
+    }
+    /*
+    c = 0;
+    while(c < 2) // recieve key and name info from children
+    {
+        byte_count = recv(pipefd[0], buf, 260, 0);
+        strcpy(names[c], buf);
+        i = strlen(names[c]);
+        memcpy(&keys[c], &buf[i+1], 4);
+        keys[c] = keys[c] & 1023;
+        c++;
+    }
+    */
+    printf("Parent recieved key information for clients from children:\n");
+    for(c=0; c<2; c++)
+    {
+        printf("CLIENT %s: %u\n", names[c], keys[c]);
+    }
+    
+    //close(pipefd[0]);
+    //close(pipefd[1]);
+    close(listener);
+    
+    fd_set reading;
+    FD_ZERO(&reading);
+    FD_SET(socks[0], &reading);
+    FD_SET(socks[1], &reading);
+    
+    int max = socks[0];
+    if(socks[1] > socks[0])
+        max = socks[1];
+        
+    printf("LISTENING FOR CONNECTION REQUEST\n");
+    int ready = select(max+1, &reading, NULL, NULL, NULL);
+    int sender, reciever;
+    if(FD_ISSET(socks[0], &reading))
+    {
+        sender = 0;
+        reciever = 1;
+    }
+    else
+    {
+        sender = 1;
+        reciever = 0;
+    }
+    byte_count = recv(socks[sender], buf, 520, 0);
+    printf("RECIEVED %d bytes\n", byte_count);
+    
+    char send_c[256]; 
+    char recv_c[256];
+    u_int nonce;
+    strcpy(send_c, buf);
+    //printf("RECIEVED SENDER ID %s\n", send_c);
+    i = strlen(send_c);
+    strcpy(recv_c, &buf[i+1]);
+    //printf("RECIEVED RECIEVER ID %s\n", recv_c);
+    c = strlen(recv_c);
+    
+    memcpy(&nonce, &buf[i+c+2], 4);
+    printf("RECIEVED NONCE VAL %u\n", nonce); 
+    // should do some verification here, don't currently
+    
+    u_int session = rand();
+    printf("SESSION KEY: %u\n", session);
+    unsigned char encrypt_buf[1024];
+    memset(encrypt_buf, 0, 200);
+    
+    memcpy(buf, &session, 4);
+    strcpy(&buf[4], recv_c);
+    memcpy(&buf[5+c], &nonce, 4);
+    memcpy(&buf[9+c], &session, 4);
+    strcpy(&buf[13+c], send_c);
+    memcpy(&buf[14+c+i], &nonce, 4);
+    
+    do_encrypt((char *) &buf[9+c], (char* ) &encrypt_buf[9+c], i+9, keys[reciever]);
+    for(j = 0; j< 18+i+c; j++)
+    {
+        printf("%u ", buf[j]);
+    }
+    printf("\n");
+    
+    memcpy(&buf[9+c], &encrypt_buf[9+c], i+9);
+    printf("\n");
+    do_encrypt((char *) buf, (char *) encrypt_buf, i+c+18, keys[sender]);
+    for(j = 0; j< 18+i+c; j++)
+    {
+        printf("%u ", encrypt_buf[j]);
+    }
+    send(socks[sender], encrypt_buf, i+c+18, 0);
+    printf("SENT ENCRYPTED MESSAGE\n");
+    
+    return 0;
+}
